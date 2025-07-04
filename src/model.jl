72918e8062f46769acccbb85fc40848c210642d7
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2025-07-04 14:32:07 +0200

    Resolve errors in model forward pass

--- src/model.jl
+++ src/model.jl
@@ -16,7 +16,7 @@ end
 
 function initialize(vector_dims::Int64, input_words::Int64, word_counts::Vector{Int64}, input_subwords::Int64, senses::Int64, tree_nodes::Int64, alpha::Float32)
   in_subwords = (rand(Float32, input_subwords, vector_dims) .- 0.5) ./ vector_dims
-  in_senses = (rand(Float32, input_words, senses, vector_dims) .- 0.5) ./ vector_dims
+  in_senses = (rand(Float32, input_words, vector_dims, senses) .- 0.5) ./ vector_dims
   out = (rand(Float32, vector_dims, tree_nodes) .- 0.5) ./ vector_dims
   as = ones(Float32, input_words, senses)
   as[:, 1] += word_counts
@@ -25,8 +25,8 @@ function initialize(vector_dims::Int64, input_words::Int64, word_counts::Vector{
   return model
 end
 
-function prototypes(model::Parameters, input_word::Int64, input_subwords::Vector{Int64})::Tuple{Array{Float32, 2}, Array{Float32, 2}}
-  latent_representation = model.in_senses[input_word, :, :] + sum(model.in_subwords[input_subwords, :], dims=2)
+function forward_pass(model::Parameters, input_word::Int64, input_subwords::Vector{Int64})::Tuple{Array{Float32, 2}, Array{Float32, 2}}
+  latent_representation = model.in_senses[input_word, :, :]' .+ sum(model.in_subwords[input_subwords, :], dims=1)
   output = latent_representation * model.out
   return latent_representation, output
 end
@@ -35,11 +35,11 @@ function sense_likelihoods(model::Parameters)::Array{Float32, 2}
   words, senses = size(model.as)
   likelihoods = zeros(Float32, words, senses)
   for sense in 1:senses
-    likelihoods[:, sense] = as[:, sense] / (as[:, sense] + bs[:, sense])
+    likelihoods[:, sense] = as[:, sense] ./ (as[:, sense] .+ bs[:, sense])
   end
   prod = ones(Float32, words)
   for sense in 1:(senses-1)
-    prod *= bs[:, sense] / (as[:, sense] + bs[:, sense])
+    prod *= bs[:, sense] ./ (as[:, sense] .+ bs[:, sense])
     likelihoods[:, sense+1] *= prod
   end
   return likelihoods
