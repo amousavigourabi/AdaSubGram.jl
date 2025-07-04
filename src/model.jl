9ea0cf5126a82540dfc18fb31824775181bfa778
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2025-07-04 13:44:02 +0200

    Add prototype and sense expectation inference

--- src/model.jl
+++ src/model.jl
@@ -18,11 +18,31 @@ function initialize_model(vector_dims::Int64, input_words::Int64, word_counts::V
   in_subwords = rand(Float32, input_subwords, vector_dims) .- 0.5 ./ vector_dims
   in_senses = rand(Float32, input_words, senses, vector_dims) .- 0.5 ./ vector_dims
   out = rand(Float32, vector_dims, tree_nodes) .- 0.5 ./ vector_dims
-  as = Array{Float32}(1.0, input_words, senses)
+  as = ones(Float32, input_words, senses)
   as[1, :] += word_counts
   bs = Array{Float32}(alpha, input_words, senses)
   model = Parameters(in_subwords, in_senses, out, as, bs)
   return model
+end
+
+function prototypes(model::Parameters, input_word::Int64, input_subwords::Vector{Int64})::Tuple{Array{Float32, 2}, Array{Float32, 2}}
+  latent_representation = model.in_senses[input_word, :, :] + sum(model.in_subwords[input_subwords, :], dims=2)
+  output = latent_representation * model.out
+  return latent_representation, output
+end
+
+function sense_likelihoods(model::Parameters)::Array{Float32, 2}
+  words, senses = size(model.as)
+  likelihoods = zeros(Float32, words, senses)
+  for sense in 1:senses
+    likelihoods[:, sense] = as[:, sense] / (as[:, sense] + bs[:, sense])
+  end
+  prod = ones(Float32, words)
+  for sense in 1:(senses-1)
+    prod *= bs[:, sense] / (as[:, sense] + bs[:, sense])
+    likelihoods[:, sense+1] *= prod
+  end
+  return likelihoods
 end
 
 export initialize_model
