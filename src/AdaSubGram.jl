5e4d21024f44e9ea1e4b4de461e580b76086e22c
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2025-07-04 13:08:48 +0200

    Add model initialization routine

--- src/AdaSubGram.jl
+++ src/AdaSubGram.jl
@@ -3,6 +3,7 @@ module AdaSubGram
 include("./hashing.jl")
 include("./preprocessing.jl")
 include("./dataset.jl")
+include("./model.jl")
 
 # Write your package code here.
 
--- src/model.jl
+++ src/model.jl
@@ -1,9 +1,30 @@
 module Model
 
-mutable struct Parameters
-  theta_subwords::Array{Float32, 2}
-  theta_senses::Array{Float32, 3}
-  betas::Array{Float32, 2}
+"""
+    Parameters(in_subwords::Array{Float32, 2}, in_senses::Array{Float32, 3}, out::Array{Float32, 2}, as::Array{Float32, 2}, bs::Array{Float32, 2})
+
+Keeps track of and maintains the model
+parameters that we are optimizing.
+"""
+struct Parameters
+  in_subwords::Array{Float32, 2}
+  in_senses::Array{Float32, 3}
+  out::Array{Float32, 2}
+  as::Array{Float32, 2}
+  bs::Array{Float32, 2}
 end
+
+function initialize_model(vector_dims::Int64, input_words::Int64, word_counts::Vector{Int64}, input_subwords::Int64, senses::Int64, tree_nodes::Int64, alpha::Float32)
+  in_subwords = rand(Float32, input_subwords, vector_dims) .- 0.5 ./ vector_dims
+  in_senses = rand(Float32, input_words, senses, vector_dims) .- 0.5 ./ vector_dims
+  out = rand(Float32, vector_dims, tree_nodes) .- 0.5 ./ vector_dims
+  as = Array{Float32}(1.0, input_words, senses)
+  as[1, :] += word_counts
+  bs = Array{Float32}(alpha, input_words, senses)
+  model = Parameters(in_subwords, in_senses, out, as, bs)
+  return model
+end
+
+export initialize_model
 
 end
