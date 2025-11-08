782b1af2c469ef035d866ca92e0ea9c9f6188889
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2025-11-09 00:38:27 +0100

    Add constant for data path

--- src/experiments.jl
+++ src/experiments.jl
@@ -1,13 +1,30 @@
 module Experiments
 
 using AdaSubGram
+using LinearAlgebra
+using DataStructures
+
+const DATA="C:\\Users\\atour\\Documents\\Projects\\AdaSubGram\\data"
 
 function generate_embeddings(handles::Vector{String})::Nothing
   for handle in handles
-    AdaSubGram.create_encodings("C:\\Users\\atour\\Documents\\Projects\\AdaSubGram\\data\\in\\$(handle).txt", "C:\\Users\\atour\\Documents\\Projects\\AdaSubGram\\data\\out\\$(handle).vec")
+    AdaSubGram.create_encodings("$(DATA)\\in\\$(handle).txt", "$(DATA)\\out\\$(handle).vec")
   end
 end
 
-export generate_embeddings
+function get_closest(handle::String, form::String)::Nothing
+  encodings = AdaSubGram.Import.read_encodings("$(DATA)\\out\\$(handle).vec")
+  compare_vector = encodings[form]
+  minheap = MutableBinaryMinHeap{Tuple{Float32, String}, DataStructures.FasterForward}()
+  for (label, vector) in encodings
+    cosine_similarity = dot(vector, compare_vector)
+    push!(minheap, (cosine_similarity, label))
+  end
+  for i in 1:10
+    println("$(i)th is $(popmin!(minheap[2])).")
+  end
+end
+
+export generate_embeddings, get_closest
 
 end
